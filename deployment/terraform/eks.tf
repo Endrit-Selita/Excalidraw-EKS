d8ac910ab3d2eff5a957a58e8f199a86a733a5f5
Endrit-Selita <eselita2001@gmail.com>
2026-02-12 02:08:11 +0000

    Add: terraform fmt to clean up code on eks.tf - /deployment/terraform/eks.tf

--- deployment/terraform/eks.tf
+++ deployment/terraform/eks.tf
@@ -1,20 +1,32 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "21.15.1"
 
-  name = local.name
+  name               = local.name
   kubernetes_version = "1.33"
 
-  endpoint_public_access = true
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  endpoint_public_access       = true
   endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
-  enable_irsa = true # I am roles for service account, allows us to create i am roles and associate them to service accounts on k8s
+  enable_irsa                              = true # I am roles for service account, allows us to create i am roles and associate them to service accounts on k8s
+  enable_cluster_creator_admin_permissions = true
 
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.public_subnets
 
-    # EKS Managed Node Group(s)
+  # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     example = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
@@ -27,5 +39,6 @@ module "eks" {
     }
   }
 
+  tags = local.tags
 
-  }
+}
