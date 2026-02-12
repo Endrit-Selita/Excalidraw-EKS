6a0c8ddc01b83aa040901a3ac238a83fdbb7e18a
Endrit-Selita <eselita2001@gmail.com>
2026-02-12 23:43:26 +0000

    Format: tf formated providers.tf - /deployment/terraform/providers.tf

--- deployment/terraform/providers.tf
+++ deployment/terraform/providers.tf
@@ -2,23 +2,23 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    bucket = "excalidraw-eks-project"
-    key = "terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
+    bucket       = "excalidraw-eks-project"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
     use_lockfile = true
     # enabled version on the aws console
   }
 
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
 
     helm = {
-        source = "hashicorp/helm"
-        version = ">= 2.6"
+      source  = "hashicorp/helm"
+      version = ">= 2.6"
     }
   }
 }
