38c7dcdc5cf611804b5d89c6abfcf7e23f84d656
Endrit-Selita <eselita2001@gmail.com>
2026-02-12 23:03:03 +0000

    Add: cert manager irsa to irsa.tf - /deployment/terraform/irsa.tf

--- deployment/terraform/irsa.tf
+++ deployment/terraform/irsa.tf
@@ -1,9 +1,19 @@
 # Cert Manager IRSA (IAM roles for service account)
 
-module "irsa" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+module "cert_manager_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  name = "Cert-manager"
+  role_name = "Cert-manager"
   attach_cert_manager_policy = true
-  cert_manager_hosted_zone_arns = 
+  cert_manager_hosted_zone_arns = local.hosted_zone_arn
 
+  oidc_providers = {
+    eks = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["Cert-manager:Cert-manager"] # name space : service account
+    }
+  }
+
+  tags = local.tags
+    
+  }
