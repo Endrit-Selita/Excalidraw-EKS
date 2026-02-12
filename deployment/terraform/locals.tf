ad84f3921986b0f877999c83cc106240c2ae2fa0
Endrit-Selita <eselita2001@gmail.com>
2026-02-12 01:32:05 +0000

    Add: terraform fmt to clean up code

--- deployment/terraform/locals.tf
+++ deployment/terraform/locals.tf
@@ -1,13 +1,13 @@
 locals {
-  name = "eks-project"
-  domain = "tahirbajramselita.co.uk"
+  name     = "eks-project"
+  domain   = "tahirbajramselita.co.uk"
   vpc_cidr = "10.0.0.0/16"
-  region = "eu-west-2"
-  cluster = "eks-excalidraw"
+  region   = "eu-west-2"
+  cluster  = "eks-excalidraw"
 
   tags = {
     Enviorment = "production"
     project    = "eks"
-    name = "Endrit"
+    name       = "Endrit"
   }
 }
