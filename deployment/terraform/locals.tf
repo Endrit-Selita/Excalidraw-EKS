0538a5b16b8a4c0609e43f4ab76377012bfbd6b7
Endrit-Selita <eselita2001@gmail.com>
2026-02-16 23:41:45 +0000

    Removed: commented out unused locals as stated by tflint error - /deployment/terraform/locals.tf

--- deployment/terraform/locals.tf
+++ deployment/terraform/locals.tf
@@ -1,9 +1,9 @@
 locals {
   name            = "eks-project"
-  domain          = "tahirbajramselita.co.uk"
+  # domain          = "tahirbajramselita.co.uk"
   vpc_cidr        = "10.0.0.0/16"
   region          = "eu-west-2"
-  cluster         = "eks-excalidraw"
+  # cluster         = "eks-excalidraw"
   hosted_zone_arn = "arn:aws:route53:::hostedzone/Z00692303651T2VZZ60MN"
 
   tags = {
